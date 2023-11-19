b6597c278600291e0dc4300bc7ca09b0feefa3e4
NikaN <NikaN7602@gmail.com>
2023-11-19 23:02:35 +0100

    Initial commit

---
+++ ci-template/src/tests/indicator.js
@@ -0,0 +1,12 @@
+function checkHealthStatus(character) {
+    const { health } = character;
+    if (health > 50) {
+      return 'healthy';
+    } else if (health >= 15) {
+      return 'wounded';
+    } else {
+      return 'critical';
+    }
+  }
+  
+  export default checkHealthStatus;
